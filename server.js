534d1a1c5464f3e790392719cbaf67f4b4754f3a
Julian Duque <julianduquej@gmail.com>
2024-10-10 13:46:12 -0500

    tweak env var

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { build } from './app.js';
 import { getLogger } from './lib/logger.js';
 
-const port = +process.env.PORT || 3000;
+const port = +process.env.APP_PORT || 3000;
 
 const server = await build({
   logger: getLogger(),
