dd1d1e76b23b49487a0dfa9fb81052d3a4418972
Daniel <daniel@stackfive.io>
2025-03-31 14:04:16 -0700

    removed  migration files

--- data/migrations/001.do.create-system-components-table.sql
+++
@@ -1,8 +0,0 @@
--- Create the 'system_components' table
-CREATE TABLE IF NOT EXISTS system_components (
-    id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
-    system_id TEXT NOT NULL,
-    FOREIGN KEY (system_id) REFERENCES systems(id),
-    name TEXT NOT NULL,
-    active BOOLEAN NOT NULL DEFAULT false
-)
--- data/migrations/002.do.add-systems-battery-storage.sql
+++
@@ -1,3 +0,0 @@
--- Add 'battery_storage' field to systems table
-ALTER TABLE systems 
-ADD battery_storage INTEGER NOT NULL DEFAULT 0
